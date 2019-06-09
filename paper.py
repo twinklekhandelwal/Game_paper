7f7ec333cad22d1a9953f91a220ed70ba27eb8e4
twinkle <you@example.com>
2019-06-09 22:22:03 +0530

    update

--- paper.py
+++ paper.py
@@ -1,6 +1,27 @@
 import random
 a=["paper","sisor","rock"]
 list_computer=(random.choice(a))
-user=raw_input("enter input")
-if(user==list_computer):
-    print "yes"
+while True:
+    user=raw_input("enter input")
+    if(user=="paper" and list_computer=="rock"):
+
+        print  "paper win"
+    elif(user=="sisores" and list_computer=="paper"):  
+        print "sisores win"
+
+    elif(user=="rock" and list_computer=="sisores"):
+        print "rock win"    
+
+    elif(user=="rock" and list_computer=="paper"):
+        print "rock loose"    
+    
+    elif(user=="paper" and list_computer=="sisores"):
+        print "paper loose"  
+
+    elif(user=="sisores" and list_computer=="rock"):
+        print "sisores loose"  
+    elif(user==list_computer):
+        print "drow"    
+      
+      
+
